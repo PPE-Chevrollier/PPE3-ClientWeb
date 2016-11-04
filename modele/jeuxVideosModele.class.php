4d1c8bb90ff92ee7bdccb75f85cabaf3a37d0dc2
quent <quent@Quentin-PC>
2016-11-04 21:23:25 +0100

    Trie par colone et asc desc

--- modele/jeuxVideosModele.class.php
+++ modele/jeuxVideosModele.class.php
@@ -24,16 +24,16 @@ class jeuxVideosModele {
 		return $nb; // si nb =1 alors l'insertion s est bien passee
 	}
 	public function getID($nomjv,$anneesortie) {
-		// recupere l'id du jeux videos correspondant �  au nom et � l'ann�e de sortie
+		// recupere l'id du jeux videos correspondant ?  au nom et ? l'ann?e de sortie
 		if ($this->idcJV) {
 			$req= "SELECT idjv from jeuxvideos where nomjv=" . $nomjv . " and anneesortie=". $anneesortie . ";" ;
 			$resultID = $this->idcJV->query($req);
 			return $resultID;
 		}
 	}
-        
+        /*
 	public function getJeuxVideoS() {
-		// recupere TOUS LES jeux vid�os de la BDD
+		// recupere TOUS LES jeux vid?os de la BDD
 		if ($this->idcJV) {
                     
                     $req ="SELECT DISTINCT je.IDJV,
@@ -52,10 +52,10 @@ class jeuxVideosModele {
                     return $resultJV;
 		}
 	}
-        
-        public function getJeuxVideoSGenres($idGenres) {
+        */
+        public function getJeuxVideos($idGenres, $colone, $sens) {
             if ($this->idcJV) {
-
+                
                 $req ="
                 SELECT DISTINCT je.IDJV,
                 NOMJV,
@@ -66,20 +66,30 @@ class jeuxVideosModele {
                 INNER JOIN genre g ON c.IDGENRE = g.IDGENRE
                 INNER JOIN jeuxvideos j ON c.IDJV = j.IDJV
                 WHERE j.IDJV = je.IDJV
-                GROUP BY j.IDJV) AS \"GENRE\"
-                FROM jeuxvideos je
-                LEFT JOIN correspondre c ON je.IDJV = c.IDJV
-                WHERE";
+                GROUP BY j.IDJV) AS \"GENRE\",
+                (SELECT ROUND(AVG(NOTE), 1)
+                FROM commentaire c
+                INNER JOIN jeuxvideos j ON c.IDJV = j.IDJV
+                WHERE j.IDJV = je.IDJV AND c.validation = 1
+                GROUP BY j.IDJV) AS \"NOTE\"
+                FROM jeuxvideos je";
 
-                $nb = count($idGenres);
-                $nbt = 0;
+                if ($idGenres != -1){
+                    
+                    $nb = count($idGenres);
+                    $nbt = 0;
 
-                foreach ($idGenres as $genre){
-                    $nbt++;
-                    $req .= ' c.IDGENRE LIKE '.$genre;
-                    if (($nb-1) == $nbt) $req.= ' OR';
+                    $req .= ' LEFT JOIN correspondre ce ON je.IDJV = ce.IDJV WHERE';
+
+                    foreach ($idGenres as $genre){
+                        $nbt++;
+                        $req .= ' ce.IDGENRE LIKE '.$genre;
+                        if (($nb-1) == $nbt) $req.= ' OR';
+                    }                
                 }
-
+                
+                $req .= ' ORDER BY UPPER('.$colone.') '.$sens;
+                
                 $resultJV = $this->idcJV->query($req);
                 return $resultJV;		
             }
